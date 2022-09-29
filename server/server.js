d57239c005e4086b7962a1784292e3a12f85172f
munna-aziz-hridoy <munna.aziz.hridoy@gmail.com>
2022-09-29 16:52:43 +0600

    changes

--- server/server.js
+++ server/server.js
@@ -3,3 +3,6 @@ import app from "./app.js";
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log("server is running"));
+
+// all games api = http://localhost:5000/api/v1/games/all-games
+// all companies api = http://localhost:5000/api/v1/company/all-companies
