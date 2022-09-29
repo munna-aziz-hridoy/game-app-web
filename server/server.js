cc8dffacbf4f13247a7e5f05a5d09eb9bc51e5e6
munna-aziz-hridoy <munna.aziz.hridoy@gmail.com>
2022-09-29 17:41:33 +0600

    test

--- server/server.js
+++ server/server.js
@@ -4,5 +4,5 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log("server is running"));
 
-// all games api = http://localhost:5000/api/v1/games/all-games
+// all games api = http://localhost:5000/api/v1/games/all-games this is the api to get all the games data
 // all companies api = http://localhost:5000/api/v1/company/all-companies
